ddc9b78a444d95790c8fc4ecbb1fd966eeb3c4e7
Raul Santana <ti.raul77@gmail.com>
2024-04-16 22:12:46 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,21 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 function Home() {
-  return ( 
+  return (
     <div>
       <Header />
-    </div> 
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE ',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+    </div>
   );
 }
 
