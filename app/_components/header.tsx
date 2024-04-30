ee8d903ed8edbd3b20d0068b3f5da2965d94283e
Raul Santana <ti.raul77@gmail.com>
2024-04-29 22:39:22 -0300

    fix: ensure the side menu opens correctly

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -12,25 +12,16 @@ const Header = () => {
   return (
     <header>
       <Card>
-        <CardContent className="p-5 justify-between items-center flex flex-row">
+        <CardContent className="p-5 justify-between flex flex-row items-center">
           <Link href="/">
-            <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+            <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
           </Link>
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="outline" size="icon">
-                <MenuIcon size={16} />
-              </Button>
-            </SheetTrigger>
 
-            <SheetContent className="p-0">
-              <SideMenu>
-                <Button variant="outline" size="icon" className="h-8 w-8">
-                  <MenuIcon size={18} />
-                </Button>
-              </SideMenu>
-            </SheetContent>
-          </Sheet>
+          <SideMenu>
+            <Button variant="outline" size="icon" className="h-8 w-8">
+              <MenuIcon size={18} />
+            </Button>
+          </SideMenu>
         </CardContent>
       </Card>
     </header>
