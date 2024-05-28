e2d02de278535e886372be07d8b8a3fbf38d7325
Raul Santana <ti.raul77@gmail.com>
2024-05-27 21:49:10 -0300

    feat: add desktop layout for header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,24 +3,87 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { CalendarDays, CircleUserRound, MenuIcon } from "lucide-react";
 import SideMenu from "./side-menu";
 import Link from "next/link";
+import { useRouter, usePathname } from "next/navigation";
+import { signIn, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Search from "../(home)/_components/search";
 
 const Header = () => {
+  const { data } = useSession();
+  const router = useRouter();
+  const currentRoute = usePathname()
+
+  function handleClickBookingsButton() {
+    router.push("/bookings");
+  }
+
+  function handleClickLoginButton() {
+    signIn("google");
+  }
+
   return (
     <header>
       <Card>
-        <CardContent className="p-5 justify-between flex flex-row items-center">
+        <CardContent className="p-5 lg:px-32 lg:gap-11 justify-between flex flex-row items-center">
           <Link href="/">
             <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
           </Link>
 
           <SideMenu>
-            <Button variant="outline" size="icon" className="h-8 w-8">
+            <Button variant="outline" size="icon" className="lg:hidden h-8 w-8">
               <MenuIcon size={18} />
             </Button>
           </SideMenu>
+
+
+          {currentRoute !== '/' && (
+            <div className="flex-1 max-w-[935px]">
+              <Search />
+            </div>
+          )}
+
+          <div className="hidden lg:flex items-center">
+            <Button
+              variant="ghost"
+              className="flex gap-2"
+              onClick={() => handleClickBookingsButton()}
+            >
+              <CalendarDays size={20} />
+              Agendamentos
+            </Button>
+            {data?.user ? (
+              <SideMenu>
+                <Button
+                  variant="ghost"
+                  className="flex gap-2 font-bold hover:bg-transparent"
+                >
+                  <Avatar className="bg-secondary flex justify-center items-center max-w-9 max-h-9">
+                    <AvatarImage
+                      src={data?.user?.image ?? ""}
+                      alt="Profile image"
+                    />
+                    <AvatarFallback className="text-primary">
+                      <h2 className="text-lg">
+                        {data?.user?.name?.charAt(0).toUpperCase()}
+                      </h2>
+                    </AvatarFallback>
+                  </Avatar>
+                  {data?.user?.name}
+                </Button>
+              </SideMenu>
+            ) : (
+              <Button
+                className="flex gap-2 font-bold"
+                onClick={() => handleClickLoginButton()}
+              >
+                <CircleUserRound size={20} />
+                Fazer Login
+              </Button>
+            )}
+          </div>
         </CardContent>
       </Card>
     </header>
